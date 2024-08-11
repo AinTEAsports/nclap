17f2b72ace215f8e9c9bbf2c85c212ae5df792dd
AinTea <lucrafteur61@gmail.com>
2024-08-11 20:21:38 +0200

    removed the 'add' example in src/nclap.nim

--- src/nclap.nim
+++ src/nclap.nim
@@ -2,9 +2,4 @@
 # exports the main API in this file. Note that you cannot rename this file
 # but you can remove it if you wish.
 
-import nclap/arguments
 import nclap/parser
-
-proc add*(x, y: int): int =
-  ## Adds two numbers together.
-  return x + y
